e6d122ed6fdbad9b48bc9bb80df3f5b974d9eebf
Maxim <developement.future@gmail.com>
2021-10-11 02:24:37 +0200

    implémentation input du dropdown

--- js/components/dropdown.js
+++ js/components/dropdown.js
@@ -14,8 +14,8 @@ const createDropdown = (name1, name2, name3) => {
     return zoneDropdown.innerHTML = `  <div class="selectContainer">
                                             <div role="button" aria-haspopup="listbox" aria-expanded class="dropdown dropdownBlue bg-primary" name="sort_by" id="sort_by">
                                                 <div class="dropFirst">
-                                                    <p>${name1}</p>
-                                                    <input type="text" class="inputInDropdown displayNone">
+                                                    <p class="name1">${name1}</p>
+                                                    <input type="text" class="inputInDropdownBlue displayNone">
                                                     <i class="bi bi-chevron-down arrowClose"></i>
                                                 </div>
                                                 <div class="optionContainer bg-primary dropdownIngredients displayNone" role="listbox"></div>
@@ -24,8 +24,8 @@ const createDropdown = (name1, name2, name3) => {
                                         <div class="selectContainer">
                                             <div role="button" aria-haspopup="listbox" aria-expanded class="dropdown dropdownGreen " name="sort_by" id="sort_by">
                                                 <div class="dropFirst">
-                                                    <p>${name2}</p>
-                                                    <input type="text" class="inputInDropdown displayNone">
+                                                    <p class="name2">${name2}</p>
+                                                    <input type="text" class="inputInDropdownGreen displayNone">
                                                     <i class="bi bi-chevron-down arrowClose"></i>
                                                 </div>
                                                 <div class="optionContainer  dropdownAppliance displayNone" role="listbox"></div>
@@ -34,8 +34,8 @@ const createDropdown = (name1, name2, name3) => {
                                         <div class="selectContainer">
                                             <div role="button" aria-haspopup="listbox" aria-expanded class="dropdown dropdownSalmon " name="sort_by" id="sort_by">
                                                 <div class="dropFirst">
-                                                    <p>${name3}</p>
-                                                    <input type="text" class="inputInDropdown displayNone">
+                                                    <p class="name3">${name3}</p>
+                                                    <input type="text" class="inputInDropdownSalmon displayNone">
                                                     <i class="bi bi-chevron-down arrowClose"></i>
                                                 </div>
                                                 <div class="optionContainer  dropdownUstensil displayNone" role="listbox"></div>
@@ -80,27 +80,36 @@ const dropdownGreen = document.querySelector(".dropdownGreen")
 const dropApp = document.querySelector(".dropdownAppliance")
 const dropdownSalmon = document.querySelector(".dropdownSalmon")
 const dropdownUst = document.querySelector(".dropdownUstensil")
+const inputInDropdownBlue = document.querySelector(".inputInDropdownBlue")
+const inputInDropdownGreen = document.querySelector(".inputInDropdownGreen")
+const inputInDropdownSalmon = document.querySelector(".inputInDropdownSalmon")
+const name1 = document.querySelector(".name1")
+const name2 = document.querySelector(".name2")
+const name3 = document.querySelector(".name3")
 
-
-const eventDropdown = (dropdownBlue, dropdownIng) => {
+const eventDropdown = (dropdownBlue, dropdownIng, inputInDropdownBlue, name1) => {
     dropdownBlue.addEventListener('click', (e) => {
         console.log(e.target);
         if (dropdownIng.classList.contains("displayNone")) {
             dropdownIng.classList.remove('displayNone')
             dropdownIng.classList.add('displayBlock')
-
-            // arrowInput.classList.add('arrowOpen')
-            // arrowInput.classList.remove('arrowClose')
+            name1.classList.add('displayNone')
         } else {
             dropdownIng.classList.remove('displayBlock')
             dropdownIng.classList.add('displayNone')
+            name1.classList.remove('displayNone')
+        }
 
-            // arrowInput.classList.remove('arrowOpen')
-            // arrowInput.classList.add('arrowClose')
+        if (inputInDropdownBlue.classList.contains("displayNone")) {
+            inputInDropdownBlue.classList.remove('displayNone')
+            inputInDropdownBlue.classList.add('displayBlock')
+        } else {
+            inputInDropdownBlue.classList.remove('displayBlock')
+            inputInDropdownBlue.classList.add('displayNone')
         }
     })
 }
 
-eventDropdown(dropdownBlue, dropdownIng)
-eventDropdown(dropdownGreen, dropApp)
-eventDropdown(dropdownSalmon, dropdownUst)
+eventDropdown(dropdownBlue, dropdownIng, inputInDropdownBlue, name1)
+eventDropdown(dropdownGreen, dropApp, inputInDropdownGreen, name2)
+eventDropdown(dropdownSalmon, dropdownUst, inputInDropdownSalmon, name3)
